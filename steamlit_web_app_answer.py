a9e57fc490a752fff7764f842b30ceb5ab8da199
塩入翔也 <shioirishouya@shioirishouyanoMacBook-Pro.local>
2025-03-06 15:34:01 +0900

    微修正

--- steamlit_web_app_answer.py
+++ steamlit_web_app_answer.py
@@ -5,8 +5,6 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')  # Streamlit でのバックエンド指定
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 import streamlit as st
